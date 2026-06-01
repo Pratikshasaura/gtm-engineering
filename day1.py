11bfba24a2ab27a5d9ed3272a187b11d67d35f3c
Pratiksha <pratikshasaura@gmail.com>
2026-06-01 21:27:54 +0100

    Add day1 file

---
+++ day1.py
@@ -0,0 +1,16 @@
+#Variable data type
+#Variable
+# String, int, float, booleadn
+#functions Print(), Type(), Input()
+
+#Variable
+# GTM example: In a CRM, a contact record holds data like first_name, company, deal_value. 
+# Variables in Python work exactly like CRM fields — each one is a named box holding a piece of information.
+company_name = "ACME Corp" #str variable
+Monthly_revenue = 42000 #Int
+deal_value = 8560.50 # decimal which is float
+is_enterprise = True #bool
+print(company_name)
+
+
+
